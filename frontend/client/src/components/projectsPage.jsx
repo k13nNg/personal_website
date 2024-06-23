3c7c5f06a59ba14f024285485b5791eeba00542d
Kien Nguyen <nguyenhuutrungkien2105@gmail.com>
2024-06-23 15:43:23 -0400

    Finished projectsPage: AddedonClick functionality to filterBox

--- frontend/client/src/components/projectsPage.jsx
+++ frontend/client/src/components/projectsPage.jsx
@@ -1,6 +1,29 @@
 import "../styles/projectsPage.css";
 
+
+
 const ProjectsPage = (props) => {
+
+    function filterField(e) {
+        e.preventDefault();
+
+        window.alert(e.target.id);
+    }
+
+    function projectNav(e) {
+        e.preventDefault();
+
+        let parentNodeCheck_1 = e.target.parentNode.id
+        let parentNodeCheck_2 = e.target.parentNode.parentNode.id
+
+        if (parentNodeCheck_1 != "") {
+            window.open(parentNodeCheck_1, "_blank")
+        } else  {
+            window.open(parentNodeCheck_2, "_blank")
+        }
+
+    }
+
     return (
         <section className="projectsPage" id="projectsPage">
             <div className="projectsPageTitle">
@@ -11,16 +34,16 @@ const ProjectsPage = (props) => {
             <div className="projectsPageContainer">
                 <div className="filter">
                     <div className="filterDesktop">
-                        <div className="filterBox filterBoxActive">
+                        <div className="filterBox filterBoxActive" id = "Full Stack Development" onClick={filterField}>
                             Full Stack Development
                         </div>
-                        <div className="filterBox">
+                        <div className="filterBox" id = "Data Analysis" onClick={filterField}>
                             Data Analysis
                         </div>
-                        <div className="filterBox">
+                        <div className="filterBox" id = "Embedded Development" onClick={filterField}>
                             Embedded Development
                         </div>
-                        <div className="filterBox">
+                        <div className="filterBox" id = "Game Development" onClick={filterField}>
                             Game Development
                         </div>
                         
@@ -34,10 +57,11 @@ const ProjectsPage = (props) => {
                             <option value="game">Game Development</option>
                         </select>
                     </div>
+                    <p style={{textAlign: "center", paddingTop: "50px"}}><i>Click on the project card to see more details!</i></p>
                 </div>
 
                 <div className="projects">
-                    <div className="projectCard">
+                    <div className="projectCard" id="https://www.google.com" onClick={projectNav}>
                         <h1 className="projectTitle">Test title</h1>
                         <div className="projectTitleDivider"></div>
                         <div className="thumbnail_and_details">
@@ -56,7 +80,7 @@ const ProjectsPage = (props) => {
                         </div>
                     </div>
 
-                    <div className="projectCard">
+                    <div className="projectCard" id="https://www.google.com" onClick={projectNav}>
                         <h1 className="projectTitle">Test title</h1>
                         <div className="projectTitleDivider"></div>
                         <div className="thumbnail_and_details">
@@ -75,7 +99,7 @@ const ProjectsPage = (props) => {
                         </div>
                     </div>
 
-                    <div className="projectCard">
+                    <div className="projectCard" id="https://www.google.com" onClick={projectNav}>
                         <h1 className="projectTitle">Test title</h1>
                         <div className="projectTitleDivider"></div>
                         <div className="thumbnail_and_details">
@@ -94,7 +118,7 @@ const ProjectsPage = (props) => {
                         </div>
                     </div>
 
-                    <div className="projectCard">
+                    <div className="projectCard" id="https://www.google.com" onClick={projectNav}>
                         <h1 className="projectTitle">Test title</h1>
                         <div className="projectTitleDivider"></div>
                         <div className="thumbnail_and_details">
