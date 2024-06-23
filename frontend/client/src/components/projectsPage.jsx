957f82ae775c0bc1ea143ba564704556ff1b6355
Kien Nguyen <nguyenhuutrungkien2105@gmail.com>
2024-06-23 03:08:47 -0400

    Finished developing basic template of projectsPage TO DO: Connect it with the backend to render the data properly

--- frontend/client/src/components/projectsPage.jsx
+++ frontend/client/src/components/projectsPage.jsx
@@ -3,9 +3,106 @@ import "../styles/projectsPage.css";
 const ProjectsPage = (props) => {
     return (
         <section className="projectsPage" id="projectsPage">
-            <h1 className = "codingSectionTitle">print(<span className="textCyan">"Projects:"</span>)</h1>
-            <br />
-            <div className="sectionDivider"></div>
+            <div className="projectsPageTitle">
+                <h1 className = "codingSectionTitle">print(<span className="textCyan">"Projects:"</span>)</h1>
+                <br />
+                <div className="sectionDivider"></div>
+            </div>
+            <div className="projectsPageContainer">
+                <div className="filter">
+                    <div className="filterBox">
+                        Full Stack Development
+                    </div>
+                    <div className="filterBox">
+                        Data Analysis
+                    </div>
+                    <div className="filterBox">
+                        Embedded Development
+                    </div>
+                    <div className="filterBox">
+                        Game Development
+                    </div>
+                </div>
+
+                <div className="projects">
+                    <div className="projectCard">
+                        <h1 className="projectTitle">Test title</h1>
+                        <div className="projectTitleDivider"></div>
+                        <div className="thumbnail_and_details">
+                            <div className="thumbnailContainer">
+                                This is the thumbnail container
+                            </div>
+                            <div className="detailsContainer">
+                                <div>Start Date - End Date</div>
+                                <p className="projectDescription">Description</p>
+                                <div className="skillsContainer">
+                                    <div className="skill">Skill 1</div>
+                                    <div className="skill">Skill 2</div>
+                                    <div className="skill">Skill 3</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="projectCard">
+                        <h1 className="projectTitle">Test title</h1>
+                        <div className="projectTitleDivider"></div>
+                        <div className="thumbnail_and_details">
+                            <div className="thumbnailContainer">
+                                This is the thumbnail container
+                            </div>
+                            <div className="detailsContainer">
+                                <div>Start Date - End Date</div>
+                                <p className="projectDescription">Description</p>
+                                <div className="skillsContainer">
+                                    <div className="skill">Skill 1</div>
+                                    <div className="skill">Skill 2</div>
+                                    <div className="skill">Skill 3</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="projectCard">
+                        <h1 className="projectTitle">Test title</h1>
+                        <div className="projectTitleDivider"></div>
+                        <div className="thumbnail_and_details">
+                            <div className="thumbnailContainer">
+                                This is the thumbnail container
+                            </div>
+                            <div className="detailsContainer">
+                                <div>Start Date - End Date</div>
+                                <p className="projectDescription">Description</p>
+                                <div className="skillsContainer">
+                                    <div className="skill">Skill 1</div>
+                                    <div className="skill">Skill 2</div>
+                                    <div className="skill">Skill 3</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="projectCard">
+                        <h1 className="projectTitle">Test title</h1>
+                        <div className="projectTitleDivider"></div>
+                        <div className="thumbnail_and_details">
+                            <div className="thumbnailContainer">
+                                This is the thumbnail container
+                            </div>
+                            <div className="detailsContainer">
+                                <div>Start Date - End Date</div>
+                                <p className="projectDescription">Description</p>
+                                <div className="skillsContainer">
+                                    <div className="skill">Skill 1</div>
+                                    <div className="skill">Skill 2</div>
+                                    <div className="skill">Skill 3</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
         </section>
     )
 }
