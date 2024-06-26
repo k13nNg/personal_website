d415b09cfbad91514f3622e4ebad3707b41a628a
Kien Nguyen <nguyenhuutrungkien2105@gmail.com>
2024-06-25 20:15:17 -0400

    Finished user-facing components

--- frontend/client/src/components/experiencePage.jsx
+++ frontend/client/src/components/experiencePage.jsx
@@ -1,20 +1,24 @@
 import "../styles/experiencePage.css";
 
 const ExperiencePage = (props) => {
+    function handleYearClick(e) {
+        e.preventDefault();
+
+        let yearWidgets = document.getElementsByClassName("yearWidget");
+
+        for (let yW of yearWidgets) {
+            yW.classList.remove("yearWidgetActive");
+        }
+
+        e.target.classList.add("yearWidgetActive");
+    }
+
     return (
         <section className="experiencePage" id="experiencePage">
             <h1>f<span className="textCyan">"Experience:"</span></h1>
             <br />
             <div className="sectionDivider"></div>
             <div className="experiencePageContentContainer">
-                <div className="timeline">
-                    <div className="verticalLine"></div>
-                    <div className="yearWidget yearWidgetActive">2024</div>
-                    <div className="verticalLine"></div>
-                    <div className="yearWidget">2023</div>
-                    <div className="verticalLine"></div>
-
-                </div>
                 <div className="experienceSection">
                     <div className="experienceCard">
                         <div className="experienceDate">
@@ -66,8 +70,67 @@ const ExperiencePage = (props) => {
                         </div>
                     </div>
 
+                    <div className="experienceCard">
+                        <div className="experienceDate">
+                            <h2>Jun - Sept, 2023</h2>
+                        </div>
+                        <div className="experienceCardTitle">
+                            <h1 className="jobTitle"><span className="textCyan">Job Title</span> @ <span>Company Name</span></h1>
+                            <div className="experienceTitleDivider"></div>
+                            <div className="experienceTextSection">
+                                
+                                <div className="jobDescription">
+                                    <ul>
+                                        <li>asldf;ajsd;lfkjasd;flkjsadf</li>
+                                        <li>as;dflkjasd;lfkjasdfasdfsad</li>
+                                        <li>as;dflkjasd;lfkjasdfasdfsad</li>
+                                    </ul>    
+                                </div>
+
+                                <div className="thumbnail">
+                                    Thumbnail
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+
+                    <div className="experienceCard">
+                        <div className="experienceDate">
+                            <h2>Jun - Sept, 2023</h2>
+                        </div>
+                        <div className="experienceCardTitle">
+                            <h1 className="jobTitle"><span className="textCyan">Job Title</span> @ <span>Company Name</span></h1>
+                            <div className="experienceTitleDivider"></div>
+                            <div className="experienceTextSection">
+                                
+                                <div className="jobDescription">
+                                    <ul>
+                                        <li>asldf;ajsd;lfkjasd;flkjsadf</li>
+                                        <li>as;dflkjasd;lfkjasdfasdfsad</li>
+                                        <li>as;dflkjasd;lfkjasdfasdfsad</li>
+                                    </ul>    
+                                </div>
+
+                                <div className="thumbnail">
+                                    Thumbnail
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+
+
                 </div>
                 
+                <div className="timeline">
+                    <div className="verticalLine"></div>
+                    <div className="yearWidget" id="yearWidget"onClick={handleYearClick}>2024</div>
+                    <div className="verticalLine"></div>
+                    <div className="yearWidget" onClick={handleYearClick}>2023</div>
+                    <div className="verticalLine"></div>
+
+                </div>
 
             </div>
         </section>
